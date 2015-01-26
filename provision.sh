59c22d772a880f9a5f8deaf0f3de23f680a3d505
Darin Critchlow <darincritchlow@gmail.com>
2015-01-26 04:07:21 +0000

    add git config lines

--- provision.sh
+++ provision.sh
@@ -15,6 +15,16 @@ function say {
 say "Installing handy packages"
   apt-get install -y curl git-core ftp unzip imagemagick vim colordiff gettext graphviz build-essential >/dev/null 2>&1
 
+say "Changing start directory for Terminal to shared vagrant directory"
+  echo 'cd /vagrant' >> ~/.bashrc
+
+say "Adding git config info"
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+  git config --global push.default simple
+  git config --global credential.helper cache
+  git config --global credential.helper 'cache --timeout=3600'
+  git config --global core.autocrlf false
 
 # Let this script know not to run again
 touch /var/vagrant_provision
